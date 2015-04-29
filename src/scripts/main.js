e1dd4f7c3a0bd0cacf17adc90372dbf5d9778b7f
Masato Seki <masatoseki@the-neighbourhood.com>
2015-04-29 21:59:34 +0100

    Adding velocity js and anchor link

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,10 @@
+window.jQuery = window.$ = require("jquery");
+require("velocity-animate");
+require('velocity-animate/velocity.ui');
+
+
 require('./modules/good-parallax').init();
 require('./modules/scrollValue').init();
 require('./modules/rotatingRamen').init();
 require('./modules/shops-hover-effects').init();
+require('./modules/smooth-anchor-link').init();
---
+++ src/scripts/modules/smooth-anchor-link.js
@@ -0,0 +1,19 @@
+var $ = require('jquery');
+
+var init = function() {
+    // bind click event to all internal page anchors
+    $("a[href*=#]").bind("click", function(e) {
+        // prevent default action and bubbling
+        e.preventDefault();
+        e.stopPropagation();
+        // set target to anchor's "href" attribute
+        var target = $(this).attr("href");
+        // scroll to each target
+        $(target).velocity("scroll", {
+            duration: 700,
+            easing: "ease"
+        });
+    });
+}
+
+exports.init = init;
