73eb4e3dfb373b16de85effe6a54667c0fcea3ca
Masato Seki <masatoseki@the-neighbourhood.com>
2015-04-29 01:18:54 +0100

    Adding rotating and gradient effect with scroll

--- src/scripts/modules/rotatingRamen.js
+++ src/scripts/modules/rotatingRamen.js
@@ -1,6 +1,16 @@
 var $ = require('jquery');
 
 var init = function() {
+    $(window).scroll(function() {
+        var position = $(window).scrollTop();
+        var rotation = ( position / ( $(document).height() - $(window).height() ) ) * 360;
+        var opacity  = position > $(window).height() ? 0.2 : 1 - ( ( position / $(window).height() ) * 0.8 );
+        $('.rotating-ramen').css({ 
+            'opacity'   : opacity,
+            'transform' : 'rotate(' + rotation + 'deg) translateZ(0)'
+        });
+
+    });
 }
 
 exports.init = init;
