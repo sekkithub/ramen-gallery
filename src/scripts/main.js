47fd52943466a17e861ff53aa5bc83523a92c2a6
Masato Seki <masatoseki@the-neighbourhood.com>
2015-04-29 21:09:20 +0100

    Adding hover effects & keyframe animation for arrow

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,4 @@
 require('./modules/good-parallax').init();
 require('./modules/scrollValue').init();
 require('./modules/rotatingRamen').init();
+require('./modules/shops-hover-effects').init();
---
+++ src/scripts/modules/shops-hover-effects.js
@@ -0,0 +1,10 @@
+var $ = require('jquery');
+
+var init = function() {
+    $(".shop").hover(function () {
+        $(this).toggleClass('transparent');
+        $(this).toggleClass('blur');
+    });
+}
+
+exports.init = init;
