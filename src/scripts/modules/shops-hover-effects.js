09e78da5f1a611f2feef6cce110e1db632aa1119
Masato Seki <masatoseki@the-neighbourhood.com>
2015-05-12 22:20:39 +0100

    Adding mobile layout

--- src/scripts/modules/shops-hover-effects.js
+++ src/scripts/modules/shops-hover-effects.js
@@ -1,23 +1,19 @@
 var init = function() {
-    $(".shop").hover(function () {
+    $(".shop--hover").hover(function () {
         $(this).toggleClass('blur');
     });
 
     $('.shop').mouseenter(function() {
         $(this).velocity({
-            opacity: 0.8,
-            letterSpacing: '.1em',
-            fontSize: '100px'
+            opacity: 0.8
         }, 70);
         $(this).next().stop().velocity({
             opacity: 1,
             right: '5%'
-        }, 350)
+        }, 350);
     }).mouseleave(function() {
         $(this).velocity({
-            opacity: 1,
-            letterSpacing: 0,
-            fontSize: '80px'
+            opacity: 1
         }, 70);
         $(this).next().stop().velocity({ opacity: 0, right: '10%' }, 350, function(){
             $(this).stop().velocity({ right: '0%' }, 0);
