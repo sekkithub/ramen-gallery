91277b87e564fbd258adfb3a58247774aed5df33
Masato Seki <masatoseki@the-neighbourhood.com>
2015-05-14 20:39:11 +0100

    Adding buggy-fill

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,6 @@
 window.jQuery = window.$ = require("jquery");
 var waypoints = require('waypoints');
+require('viewport-units-buggyfill').init();
 require('velocity-animate');
 require('velocity-animate/velocity.ui');
 
